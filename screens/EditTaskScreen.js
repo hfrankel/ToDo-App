186a2850324d6ac0fa039b2483bb8ec538295f4b
Harry Frankel <hfranks92@gmail.com>
2020-02-20 17:52:45 +1100

    Navigation to individual tasks working

--- screens/EditTaskScreen.js
+++ screens/EditTaskScreen.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { TextInput, StyleSheet, View } from 'react-native';
 
-const EditTaskScreen = () => {
-  return <Text>Hello World</Text>;
+const EditTaskScreen = ({ route, navigation }) => {
+  const { item } = route.params;
+  const [editedtext, setEditedText] = useState(`${item.text}`);
+
+  // Find a way to pass params through header button then update state for item.text
+  // on homescreen
+
+  return (
+    <TextInput
+      style={styles.listItemText}
+      onChangeText={text => setEditedText(text)}
+      value={editedtext}
+    ></TextInput>
+  );
 };
+
+const styles = StyleSheet.create({
+  listItemText: {
+    fontSize: 18,
+    marginTop: 6,
+    marginLeft: 6
+  }
+});
 
 export default EditTaskScreen;
